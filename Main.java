d7b86c31350cece5c8432f23b282c1a4de7a38ef
Simt'pal <simarpal.kalsi@gmail.com>
2020-11-14 12:32:30 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
-Actually bots can work, I quickly coded a simple bot that will tell me if a new listing has occurred and then it will print out the page for the new listing and then from there is you can find the redirect to the 3080 page. Not sure what types of counter-measures you guys have against simple bots like this but since you guys are using shopify perhaps there are existing plugins that help out with giveaways. Here's code if anyone interested. I still have my doubts as to if my solution would work  cause it seems far to easy to be valid. There has to be a catch. 
-
-
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;https://github.com/simar1998/ltt-storewatcher/blob/main/Main,java
+import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
